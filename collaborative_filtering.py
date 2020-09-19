23da914ddb64bac7f99ad90256bbf87faade794c
phusdt <phuduytran1007@gmail.com>
2020-09-19 19:57:49 +0700

    __init__ func

--- collaborative_filtering.py
+++ collaborative_filtering.py
@@ -11,4 +11,15 @@ warnings.filterwarnings('ignore')
 
 class CF(object):
     """Docstring for DF"""
+    def __init__(self, Y_data, k, dist_func=pearsonr):
+        self.Y_data = Y_data
+        self.k = k
+        self.dist_func = dist_func
+
+        #number of users and items. Remember to add 1 since id starts from 0
+        self.n_users = int(np.max(self.Y_data[:, 0])) + 1
+        self.n_items = int(np.max(self.Y_data[:, 1])) + 1
+
+        self.Ybar_data = None #normalized
+    
     
