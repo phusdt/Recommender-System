84991f8751414f59849069caefdce75578327368
phusdt <phuduytran1007@gmail.com>
2020-09-19 14:55:11 +0700

    start build CF

---
+++ collaborative_filtering.py
@@ -0,0 +1,11 @@
+import numpy as np
+from scipy import sparse
+from scipy.stats import pearsonr 
+from get_data import(
+    get_rating_base_data,
+    get_rating_test_data,
+)
+
+import warnings
+warnings.filterwarnings('ignore')
+
