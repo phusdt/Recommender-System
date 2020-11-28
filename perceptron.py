5186b67e30a798dbf430f8f21560c6eac4855c68
phusdt <phuduytran1007@gmail.com>
2020-11-28 17:34:39 +0700

    initation for perceptron model

---
+++ perceptron.py
@@ -0,0 +1,11 @@
+from collaborative_filtering import *
+from demographic_filtering import *
+from get_data import (
+    get_users_data,
+    get_rating_base_data,
+    get_rating_test_data,
+)
+
+
+class Perceptron:
+    def __init__(self
