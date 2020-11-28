a64c643554ca7677fa54a786a73aeb86e46ada4d
phusdt <phuduytran1007@gmail.com>
2020-11-28 17:39:30 +0700

    initation for perceptron model

--- perceptron.py
+++ perceptron.py
@@ -1,3 +1,5 @@
+import numpy as np
+import random
 from collaborative_filtering import *
 from demographic_filtering import *
 from get_data import (
@@ -6,7 +8,10 @@ from get_data import (
     get_rating_test_data,
 )
 
-
 class Perceptron:
-    def __init__(self):
-        pass
+    def __init__(self, dataset, learning_rate, n_iters):
+        self.dataset = dataset
+        self.learning_rate = learning_rate
+        self.n_iters = n_iters
+        self.w1 = random.uniform(0, 1)
+        self.w2 = random.uniform(0, 1)
