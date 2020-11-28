0534000f9044e8155faa03be3b8f346d559d1d8c
phusdt <phuduytran1007@gmail.com>
2020-11-28 17:22:28 +0700

    __init__

--- demographic_filtering.py
+++ demographic_filtering.py
@@ -13,3 +13,13 @@ class DF(object):
     """ Docstring for DF """
 
     def __init__(self, users, Y_data, k, dist_func=cosine_similarity):
+        self.users = users
+        self.Y_data = Y_data
+        self.k = k
+        self.dist_func = dist_func
+
+        # number of users and items. Remember to add 1 since id starts from 0
+        self.n_users = int(np.max(self.Y_data[:, 0])) + 1
+        self.n_items = int(np.max(self.Y_data[:, 1])) + 1
+
+        self.Ybar_data = None
