469bed667823a23290b72d329012021a8041a7ab
phusdt <phuduytran1007@gmail.com>
2020-11-28 17:40:04 +0700

    fit func in perceptron

--- perceptron.py
+++ perceptron.py
@@ -15,3 +15,19 @@ class Perceptron:
         self.n_iters = n_iters
         self.w1 = random.uniform(0, 1)
         self.w2 = random.uniform(0, 1)
+        
+    def fit(self):
+
+        n = self.dataset[0, 0] * self.w1 + self.dataset[0, 1] * self.w2
+
+        for i in range(1, self.n_iters):
+            # update weight
+            w1_stamp = self.w1 + self.learning_rate * self.dataset[i - 1, 0] * (dataset[i - 1, 2] - n)
+            w2_stamp = self.w2 + self.learning_rate * self.dataset[i - 1, 1] * (dataset[i - 1, 2] - n)
+            if np.abs(self.w1 - w1_stamp) <= 0.0001 and np.abs(self.w2 - w2_stamp) <= 0.0001:
+                break
+            else:
+                self.w1 = w1_stamp
+                self.w2 = w2_stamp
+
+            n = self.dataset[i, 0] * self.w1 + self.dataset[i, 1] * self.w2
